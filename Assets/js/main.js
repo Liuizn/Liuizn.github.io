d1f458a393033209869084f395bd83030ef09657
Liuizn <Luizgabrielsnn@gmail.com>
2021-10-04 04:48:43 -0300

    finish section 'Stack'

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,18 +1,18 @@
 /* show & Hidden Function -start*/
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
-    
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
 /*show - validando se a constante existe*/
-if(navToggle) {
-    navToggle.addEventListener('click', ()=>{
-            navMenu.classList.add('show-menu')
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
     })
 }
-    
+
 /*hidden - validanto se a constante existe*/
-if(navClose) {
-    navClose.addEventListener('click', ()=>{
+if (navClose) {
+    navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
@@ -21,9 +21,34 @@ if(navClose) {
 /*Remove Menu Mobile*/
 const navLink = document.querySelectorAll('.nav__link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+/*start -acordeão stack*/
+const stackContent = document.getElementsByClassName('stack__content')
+const stackHeader = document.querySelectorAll('.stack__header')
+
+function toggleStacks() {
+    let itemClass = this.parentNode.className
+
+    for (let i = 0; i < stackContent.length; i++) {
+        stackContent[i].className = 'stack__content stack__close'
+    }
+
+    if(itemClass === 'stack__content stack__close'){
+        this.parentNode.className = 'stack__content stack__open'
+    }
+}
+
+stackHeader.forEach((el) =>{
+    el.addEventListener('click', toggleStacks)
+})
+
+
+/*end -acordeão stack*/
