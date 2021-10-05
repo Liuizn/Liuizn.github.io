777f01641bdfc4b8d9691f216770463352d30315
Liuizn <Luizgabrielsnn@gmail.com>
2021-10-05 18:15:04 -0300

    changes in archives

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -137,4 +137,4 @@ themeButton.addEventListener('click', () => {
     // We save the theme and the current icon that the user chose
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
-})
+}) 
